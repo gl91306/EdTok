3338763b9079011c146de7d0275d1080f21f4622
Aman Nindra <amannindra@gmail.com>
2024-07-13 13:26:30 -0700

    added Login.js and main.js

--- src/App.js
+++ src/App.js
@@ -2,41 +2,27 @@ import SideBar from "./sidebar/sidebar";
 import MainBar from "./mainbar/mainbar";
 import React, { useState, useEffect } from "react";
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
-
+import MainPage from "./main"
+import LoginPage from "./login"
 import "./index.css";
 
 function App() {
   const [showDevMode, setshowDevMode] = useState(false);
   const [signin, setsignin] = useState();
-  const DevMode = () => {
-    if (showDevMode) {
-      return (
-        <>
-          <div id="horizontal"></div>
-          <div id="vertical"></div>
-        </>
-      );
-    }
-  };
-  const signinbar = () => {
-    return (
-      <div>
-        <a href="/signin.js">Sign In</a>
-      </div>
-    );
-  };
-  const nosigninbar = () => {
-    return;
-  };
   return (
-    <BrowserRouter>
-      {signin ? signinbar() : nosigninbar()}
-      
+    <>
+    
       <div className="App">
-        <SideBar showDevMode={showDevMode} DevMode={DevMode} />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path='/login' element ={<LoginPage/>}/>
+          </Routes>
+        </BrowserRouter>
+        <SideBar />
         <MainBar />
       </div>
-    </BrowserRouter>
+    </>
   );
 }
 
