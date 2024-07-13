0736745fc7365f3577ac95ec056bc5597dd8d10e
Aman Nindra <amannindra@gmail.com>
2024-07-13 03:04:19 -0700

    fixed Css

--- src/sidebar/sidebar.js
+++ src/sidebar/sidebar.js
@@ -4,26 +4,18 @@ function Sidebar(props) {
   return (
     <div className="topics">
       <div className="title">
-        <h1>EdTok</h1>
-      </div>
-      <div className="yours">
         <h1>
-        For You
+          EdTok
         </h1>
+      </div>
+      <div className="yours ">
+        <h1 >For You</h1>
       </div>
       <div className="classes">
-        <h1>
-          Math
-        </h1>
-        <h1>
-          Reading
-        </h1>
-        <h1>
-          Biology
-        </h1>
-        <h1>
-          World History
-        </h1>
+        <h1 className="lineanimation">Math</h1>
+        <h1 className="lineanimation">Reading</h1>
+        <h1 className="lineanimation">Biology</h1>
+        <h1 className="lineanimation">World History</h1>
       </div>
     </div>
   );
