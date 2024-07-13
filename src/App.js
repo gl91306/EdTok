340a39e75052b560cf2db43006149fb8ff232cb3
Aman Nindra <amannindra@gmail.com>
2024-07-13 13:29:26 -0700

    fixed connection issue

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import SideBar from "./sidebar/sidebar";
-import MainBar from "./mainbar/mainbar";
-import React, { useState, useEffect } from "react";
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
-import MainPage from "./main"
-import LoginPage from "./login"
+import MainPage from "./main";
+import LoginPage from "./login";
 import "./index.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
   const [showDevMode, setshowDevMode] = useState(false);
   const [signin, setsignin] = useState();
   return (
     <>
-    
       <div className="App">
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<MainPage />} />
-            <Route path='/login' element ={<LoginPage/>}/>
+            <Route path="/login" element={<LoginPage />} />
           </Routes>
         </BrowserRouter>
-        <SideBar />
-        <MainBar />
       </div>
     </>
   );
--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
+import SideBar from "./sidebar/sidebar";
+import MainBar from "./mainbar/mainbar";
+import React, { useState, useEffect } from "react";
 function MainPage() {
-    return(<>
-        
-    </>)
+  return (
+    <>
+      <SideBar />
+      <MainBar />
+    </>
+  );
 }
 
 export default MainPage;
