5283ebe58e8810f4edbeac8d47e67363dbe5fc73
Aman Nindra <amannindra@gmail.com>
2024-07-14 02:45:25 -0700

    comment Section CSS

--- src/comments/comments.js
+++ src/comments/comments.js
@@ -1,4 +1,29 @@
-function Comments(){
-    return <>   </>
+import "./comments.css";
+import Send from "../images/send.png";
+import profile from "../images/pfp.png";
+function Comments() {
+  return (
+    <div className="entire">
+      <div className="comment">
+        <div className="description">
+          <h1>Natural e^x derivatives</h1>
+          <p>A deep dive into exponential derivatives</p>
+        </div>
+        <div className="studentCommentDiv">
+          <div className="studentComments">
+            <div style={{ display: "flex", gap: "10px" }}>
+              <img src={profile}></img>
+              <h1>Joe Don</h1>
+            </div>
+            <p>A deep dive into exponential derivatives</p>
+          </div>
+        </div>
+      </div>
+      <div className="youcomment">
+        <input type="text" placeholder="ask!"></input>
+        <img src={Send}></img>
+      </div>
+    </div>
+  );
 }
-exportd default Comments
+export default Comments;
--- src/mainbar/mainbar.js
+++ src/mainbar/mainbar.js
@@ -2,26 +2,29 @@ import "./mainbar.css";
 import { useState } from "react";
 import comment from "../images/message.png";
 import sound from "../images/sound.png";
+import Comments from "../comments/comments";
 function Mainbar() {
-  const [comments, setcomments] = useState(false);
-  const handleComments = () =>{
-    
-  }
+  const [comments, setcomments] = useState(true);
+  const handleComments = () => {
+    setcomments(!comments);
+  };
 
   return (
     <div className="rightside">
       <div className="video"></div>
+      {comments ? <Comments /> : null}
       <div className="videobuttons">
         <div className="background">
           <div className="heart"></div>
         </div>
-        <div className="background">
+        <div className="background" onClick={handleComments}>
           <img src={comment} />
         </div>
-        <div className="background" onClick={handleComments}>
+        <div className="background">
           <img src={sound}></img>
         </div>
       </div>
+      
     </div>
   );
 }
