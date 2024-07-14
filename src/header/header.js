710a7f4a0e336c64689998cd89181c8dc53aa39b
gl91306@student.musd.org <gl91306@student.musd.org>
2024-07-13 22:15:54 -0700

    make profile absolute

--- src/header/header.js
+++ src/header/header.js
@@ -2,11 +2,9 @@ import "./header.css";
 import pfp from "../images/pfp.png"; // temp
 function Header() {
   return (
-    <div id="header">
-      <div id="profilecontainer">
-        <p id="username">john doe</p>
-        <img id="pfp" src={pfp} alt="profile" />
-      </div>
+    <div id="profilecontainer">
+      <p id="username">john doe</p>
+      <img id="pfp" src={pfp} alt="profile" />
     </div>
   );
 }
