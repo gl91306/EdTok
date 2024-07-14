e480a871550de92a51e57b8879e66da4c613afe0
Aman Nindra <amannindra@gmail.com>
2024-07-14 01:18:16 -0700

    added heart, comment, sound png

---
+++ src/comments/comments.js
@@ -0,0 +1,4 @@
+function Comments(){
+    return <>   </>
+}
+exportd default Comments
--- src/mainbar/mainbar.js
+++ src/mainbar/mainbar.js
@@ -1,8 +1,27 @@
 import "./mainbar.css";
+import { useState } from "react";
+import comment from "../images/message.png";
+import sound from "../images/sound.png";
 function Mainbar() {
+  const [comments, setcomments] = useState(false);
+  const handleComments = () =>{
+    
+  }
+
   return (
     <div className="rightside">
       <div className="video"></div>
+      <div className="videobuttons">
+        <div className="background">
+          <div className="heart"></div>
+        </div>
+        <div className="background">
+          <img src={comment} />
+        </div>
+        <div className="background" onClick={handleComments}>
+          <img src={sound}></img>
+        </div>
+      </div>
     </div>
   );
 }
